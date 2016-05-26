dd8ae1caee3ce9948d48630d2376e67b56bc05a1
Ibrahim Amin <lethalskillzz@gmail.com>
2016-05-26 16:38:57 +0100

    edit files

--- views/js/type.js
+++ views/js/type.js
@@ -1,32 +1,24 @@
 $(document ).ready(function() {
 
-var name = document.getElementById('full_name'),
-pic = document.getElementById('profile_pic'),
-time_count = document.getElementById('time_count'),
-seconds = 0, minutes = 0, hours = 0, t;
-
+var name = document.getElementById('full_name');
+pic = document.getElementById('profile_pic');
 
 name.textContent = localStorage.full_name;
 pic.src = localStorage.profile_pic;
 pic.height = 102;
 pic.width = 102;
 
-localStorage.removeItem("full_name");
-localStorage.removeItem("profile_pic");
+/*localStorage.removeItem("full_name");
+localStorage.removeItem("profile_pic");*/
 
 
 $('#start_submit').on('click',function() {
-    
-  timer();
-
-});
-
-
-function timer() {
-    t = setTimeout(add, 1000);
-}
-
-function add() {
+    alert("yeeey");
+  var time_count = document.getElementById('time_count');
+  var seconds = 0, minutes = 0, hours = 0,
+      t;
+  
+  function add() {
     seconds++;
     if (seconds >= 60) {
         seconds = 0;
@@ -39,9 +31,20 @@ function add() {
     time_count.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
     timer();
   }
+  
+  function timer() {
+    t = setTimeout(add, 1000);
+  }
+  
+  timer();
+    
+});
 
 
 /*
+// Start button 
+start.onclick = timer;
+
 // Stop button 
 stop.onclick = function() {
     clearTimeout(t);
@@ -51,7 +54,7 @@ stop.onclick = function() {
 clear.onclick = function() {
     h1.textContent = "00:00:00";
     seconds = 0; minutes = 0; hours = 0;
-} */
+}*/
 
 
 });
