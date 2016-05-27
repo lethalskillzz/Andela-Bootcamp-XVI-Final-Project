327eec7670324b28c97329d323a2d2a0dce18ddd
Ibrahim Amin <lethalskillzz@gmail.com>
2016-05-27 13:01:27 +0100

    edit files

--- views/js/index.js
+++ views/js/index.js
@@ -1,102 +1,102 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-//variable initialization
-var first_img = document.getElementById('1st_img'),
-first_name = document.getElementById('1st_name'),
-first_score = document.getElementById('1st_score'),
-second_img = document.getElementById('2nd_img'),
-second_name = document.getElementById('2nd_name'),
-second_score = document.getElementById('2nd_score'),
-third_img = document.getElementById('3rd_img'),
-third_name = document.getElementById('3rd_name'),
-third_score = document.getElementById('3rd_score');
+  //variable initialization
+  var first_img = document.getElementById('1st_img'),
+    first_name = document.getElementById('1st_name'),
+    first_score = document.getElementById('1st_score'),
+    second_img = document.getElementById('2nd_img'),
+    second_name = document.getElementById('2nd_name'),
+    second_score = document.getElementById('2nd_score'),
+    third_img = document.getElementById('3rd_img'),
+    third_name = document.getElementById('3rd_name'),
+    third_score = document.getElementById('3rd_score');
 
 
-var userRef = new Firebase("https://flickering-inferno-1776.firebaseio.com/users");
-userRef.orderByChild("speed").on("value", function(snapshot) {
-  snapshot.forEach(function(data) {
-    console.log("The " + data.key() + "score is " + data.val());
+  var userRef = new Firebase("https://flickering-inferno-1776.firebaseio.com/users");
+  userRef.orderByValue("speed").on("value", function (snapshot) {
+    snapshot.forEach(function (data) {
+      console.log("The " + data.key() + "score is " + data.val());
+    });
   });
-});
 
-//on window scroll
-$(window).scroll(function() {    
+  //on window scroll
+  $(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 55) {
-        $("#header").addClass("scrolling");
-        $("#nav a").addClass("scrolling");
+      $("#header").addClass("scrolling");
+      $("#nav a").addClass("scrolling");
     } else {
-        $("#header").removeClass("scrolling");
-        $("#nav a").removeClass("scrolling");
+      $("#header").removeClass("scrolling");
+      $("#nav a").removeClass("scrolling");
     }
-});
+  });
 
-//leaderboard tab button click
-$('#leader_board').on('click',function() {    
-  window.scrollTo(0, 1300); 
-});
+  //leaderboard tab button click
+  $('#leader_board').on('click', function () {
+    window.scrollTo(0, 1300);
+  });
 
-//about tab button click
-$('#about').on('click',function() {    
-  window.scrollTo(0, 2300); 
-});
+  //about tab button click
+  $('#about').on('click', function () {
+    window.scrollTo(0, 2300);
+  });
 
 
-// When the user clicks the button, open the modal 
-$('#modal_trigger').on('click',function() {
+  // When the user clicks the button, open the modal 
+  $('#modal_trigger').on('click', function () {
 
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-modal.style.display = "block";
+    modal.style.display = "block";
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
         modal.style.display = "none";
+      }
     }
-}
 
-});
+  });
 
-//login with google button
-$('#google_login').on('click',function() {
-  
-var ref = new Firebase("https://flickering-inferno-1776.firebaseio.com");
+  //login with google button
+  $('#google_login').on('click', function () {
 
-ref.authWithOAuthPopup("google", function(error, authData) { 
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-    var usersRef = ref.child("users");
-    var userRef = usersRef.child(authData.uid);
-    userRef.set({
-    email: authData.google.email,
-    full_name: authData.google.displayName,
-    profile_pic: authData.google.profileImageURL
-    });
-    
-    localStorage.userRef = authData.uid;
-    localStorage.full_name = authData.google.displayName;
-    localStorage.profile_pic = authData.google.profileImageURL;
-    window.location.href = "/type";
- }
-  
- }, {
-  remember: "sessionOnly",
-  scope: "email"
-});
+    var ref = new Firebase("https://flickering-inferno-1776.firebaseio.com");
 
-});  
+    ref.authWithOAuthPopup("google", function (error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        var usersRef = ref.child("users");
+        var userRef = usersRef.child(authData.uid);
+        userRef.set({
+          email: authData.google.email,
+          full_name: authData.google.displayName,
+          profile_pic: authData.google.profileImageURL
+        });
+
+        localStorage.userRef = authData.uid;
+        localStorage.full_name = authData.google.displayName;
+        localStorage.profile_pic = authData.google.profileImageURL;
+        window.location.href = "/type";
+      }
+
+    }, {
+        remember: "sessionOnly",
+        scope: "email"
+      });
+
+  });
 
 });
 
--- views/js/type.js
+++ views/js/type.js
@@ -1,119 +1,119 @@
-$(document ).ready(function() {
+$(document).ready(function () {
 
-//variable initialization
-var name = document.getElementById('full_name'),
-pic = document.getElementById('profile_pic');
-name.textContent = localStorage.full_name;
-pic.src = localStorage.profile_pic;
-pic.height = 102;
-pic.width = 102;
+    //variable initialization
+    var name = document.getElementById('full_name'),
+        pic = document.getElementById('profile_pic');
+    name.textContent = localStorage.full_name;
+    pic.src = localStorage.profile_pic;
+    pic.height = 102;
+    pic.width = 102;
 
- var oneMinute = 60,
- timer_display = document.getElementById('time_count'),
- text_output = document.getElementById('text_output'),
- text_input = document.getElementById('text_input'),
- modal = document.getElementById('result_modal'),
- result_name = document.getElementById('result_full_name'),
- result_pic = document.getElementById('result_profile_pic'),
- result_speed = document.getElementById('result_speed'),speed,
- speed = 0;
- 
- //Array of sample text
- var strToTest = new Array("Innovative Technical Solutions, LLC (ITS) is a Native" + 
-                " American owned business that was established in Paducah, " + 
-                "Kentucky in April 2000. ITS is a certified and Small Disadvantaged " + 
-                "Business by the U.S. Small Business Administration. Our headquarters " + 
-                "are in Paducah, Kentucky and we have" + 
-                " satellite offices located in Tennessee, " + 
-                "Ohio, and Illinois. ITS is a leading" + 
-                " edge Information Technology firm that " + 
-                "is comprised of professionals with a broad" + 
-                " range of experience in software " + 
-                "development, high-speed imaging/scanning (TIFF, PDF, Text, " + 
-                "and OCR capabilities), document management, records management," + 
-                " relevance management, information security, environmental " + 
-                "management, fire services management, fire protection " + 
-                "engineering, and protective force expertise.",
-                "The ITS Information Technology (IT) Team are experts " + 
-                "in the identification, capture, indexing, microfilming, imaging, " + 
-                "disposition, turnover, storage, and retrieval of records, " + 
-                "and in the administration of records management databases. " + 
-                "The types of records we have extensive experience in managing " + 
-                "include waste management, hazardous waste, waste shipment, " + 
-                "environmental compliance, environmental" + 
-                " monitoring, feasibility studies, " + 
-                "environmental work plans, cleanup actions, cemetery records, and " + 
-                "various Federal laws such as CERCLA, Paper Reduction,");
-                
- //load text to view              
- text_output.textContent = strToTest;
- 
-  //start timer
- startTimer(oneMinute, timer_display);
-  
-//click save button  
-$('#result_save').on('click',function() { 
-    var ref = new Firebase("https://flickering-inferno-1776.firebaseio.com/users");
-    var uidRef = ref.child(localStorage.userRef);
-    uidRef.update({
-     "speed": speed
+    var oneMinute = 60,
+        timer_display = document.getElementById('time_count'),
+        text_output = document.getElementById('text_output'),
+        text_input = document.getElementById('text_input'),
+        modal = document.getElementById('result_modal'),
+        result_name = document.getElementById('result_full_name'),
+        result_pic = document.getElementById('result_profile_pic'),
+        result_speed = document.getElementById('result_speed'), speed,
+        speed = 0;
+
+    //Array of sample text
+    var strToTest = new Array("Innovative Technical Solutions, LLC (ITS) is a Native" +
+        " American owned business that was established in Paducah, " +
+        "Kentucky in April 2000. ITS is a certified and Small Disadvantaged " +
+        "Business by the U.S. Small Business Administration. Our headquarters " +
+        "are in Paducah, Kentucky and we have" +
+        " satellite offices located in Tennessee, " +
+        "Ohio, and Illinois. ITS is a leading" +
+        " edge Information Technology firm that " +
+        "is comprised of professionals with a broad" +
+        " range of experience in software " +
+        "development, high-speed imaging/scanning (TIFF, PDF, Text, " +
+        "and OCR capabilities), document management, records management," +
+        " relevance management, information security, environmental " +
+        "management, fire services management, fire protection " +
+        "engineering, and protective force expertise.",
+        "The ITS Information Technology (IT) Team are experts " +
+        "in the identification, capture, indexing, microfilming, imaging, " +
+        "disposition, turnover, storage, and retrieval of records, " +
+        "and in the administration of records management databases. " +
+        "The types of records we have extensive experience in managing " +
+        "include waste management, hazardous waste, waste shipment, " +
+        "environmental compliance, environmental" +
+        " monitoring, feasibility studies, " +
+        "environmental work plans, cleanup actions, cemetery records, and " +
+        "various Federal laws such as CERCLA, Paper Reduction,");
+
+    //load text to view              
+    text_output.textContent = strToTest;
+
+    //start timer
+    startTimer(oneMinute, timer_display);
+
+    //click save button  
+    $('#result_save').on('click', function () {
+        var ref = new Firebase("https://flickering-inferno-1776.firebaseio.com/users");
+        var uidRef = ref.child(localStorage.userRef);
+        uidRef.update({
+            "speed": speed
+        });
+        window.location.href = "/";
     });
-    window.location.href = "/";
-});
 
-//timer function
-function startTimer(duration, display) {
-    var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
-        
-    function timer() {
-        // get the number of seconds that have elapsed since 
-        // startTimer() was called
-        diff = duration - (((Date.now() - start) / 1000) | 0);
+    //timer function
+    function startTimer(duration, display) {
+        var start = Date.now(),
+            diff,
+            minutes,
+            seconds;
 
-        // does the same job as parseInt truncates the float
-        minutes = (diff / 60) | 0;
-        seconds = (diff % 60) | 0;
+        function timer() {
+            // get the number of seconds that have elapsed since 
+            // startTimer() was called
+            diff = duration - (((Date.now() - start) / 1000) | 0);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+            // does the same job as parseInt truncates the float
+            minutes = (diff / 60) | 0;
+            seconds = (diff % 60) | 0;
 
-        display.textContent = minutes + ":" + seconds; 
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        if (diff <= 0) {
-            // add one second so that the count down starts at the full duration
-            // example 05:00 not 04:59
-            //start = Date.now() + 1000;
-            showScore();
-            
-        }
-    };
-    // we don't want to wait a full second before the timer starts
-    timer();
-    setInterval(timer, 1000);
-}
+            display.textContent = minutes + ":" + seconds;
 
-//prepare and display result dialog 
-function showScore() {
-  
-  result_name.textContent = localStorage.full_name;
-  result_pic.src = localStorage.profile_pic;
-  result_pic.height = 102;
-  result_pic.width = 102;
-  
-  input = text_input.value.trim();
-  
-  speed = input.replace(/  /g," ").split(" ").length;
-  result_speed.textContent = "speed: "+speed+" wpm";
-  
-  timer_display.style.visibility = 'hidden';              
-  modal.style.display = "block";
-  
-  
-}
-   
-  
-   
+            if (diff <= 0) {
+                // add one second so that the count down starts at the full duration
+                // example 05:00 not 04:59
+                //start = Date.now() + 1000;
+                showScore();
+
+            }
+        };
+        // we don't want to wait a full second before the timer starts
+        timer();
+        setInterval(timer, 1000);
+    }
+
+    //prepare and display result dialog 
+    function showScore() {
+
+        result_name.textContent = localStorage.full_name;
+        result_pic.src = localStorage.profile_pic;
+        result_pic.height = 102;
+        result_pic.width = 102;
+
+        input = text_input.value.trim();
+
+        speed = input.replace(/  /g, " ").split(" ").length;
+        result_speed.textContent = "speed: " + speed + " wpm";
+
+        timer_display.style.visibility = 'hidden';
+        modal.style.display = "block";
+
+
+    }
+
+
+
 });
