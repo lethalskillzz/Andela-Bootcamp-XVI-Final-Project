0649ef8464c8c5931075f693c72df345495cf7ef
Ibrahim Amin <lethalskillzz@gmail.com>
2016-06-04 22:02:37 +0100

    change port to 5000

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(5000,function(){
+  console.log("Live at Port 5000");
 });
